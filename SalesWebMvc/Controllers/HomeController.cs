4c5f7e85f8b3b55a97598aecc0b5f9acabee1adb
Erick Marcelo <Ems2201@outlook.com>
2020-05-23 09:35:24 -0300

    Mudando as coisas no Controller about

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,8 +13,8 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Salles Web Mvc App from C# Course.";
+            ViewData["Student"] = "Erick Marcelo";
             return View();
         }
 
